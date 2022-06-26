65437e68915048a7bce9652d1c4830d983164bd8
mohamed douzi <78613047+meddouzi@users.noreply.github.com>
2022-06-26 15:08:01 +0100

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,12 @@
+// 1. you can't define a variable more than once
+let petName = 'boob'
+
+petName = 'frexx'
+console.log(petName)
+
+// 2. there are rules related to variable names
+let test_ = 2
+let  result = 3 + 4
+
+// 3. variable names cannot be reserved keywords
+// let let = 12
